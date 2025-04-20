865a53018863f385ba3dbb3f1130e607e29df09a
Kelvin Zawadi <kelvin.vitone@gmail.com>
2025-04-20 21:27:16 +0300

    i fixed the app titile

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Attendence Manag sys',
+      title: 'Attendence Management sys',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
